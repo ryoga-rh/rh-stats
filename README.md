6601a6f24e66803367ba6a5f8abadbd1502b9a7e
ryoga rh <86823436+ryoga-rh@users.noreply.github.com>
2022-07-03 21:46:31 -0500

    * lo crea *
    primer commit xD

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # rh-stats
  Pequeña herramienta para ver las estadisticas de tu pc, como procesador, ram, memoria y de mas
+
+# No esta terminada
+
+Tiene muchos errores y el menu me falta hacerlo, lo ando haciendo pero tardare un rato xd
+
+# Iniciar
+ pip install -r requirements.txt
+ luego usa python main.py
---
+++ main.py
@@ -0,0 +1,79 @@
+from dis import dis
+from traceback import print_tb
+import psutil
+import platform
+from datetime import datetime
+import os
+from colorama import *
+import sys
+
+def limpiar():
+    if sys.platform == "win32":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+def infobasica():
+    plataforma = platform.uname()
+    limpiar()
+    print(Fore.LIGHTGREEN_EX)
+    print("<" + "=" * 40 + " Informacion Basica Del Sistema " + "=" * 40 + ">")
+    print("#")
+    print("# Sistema Operativo: ")
+    print(f"# {plataforma.system}")
+    print(f"#")
+    print(f"# Nombre del nodo: ")
+    print(f"# {plataforma.node}")
+    print(f"#")
+    print(f"# Libreria :")
+    print(f"# {plataforma.release}")
+    print(f"#")
+    print(f"# Version")
+    print(f"# {plataforma.version}")
+    print(f"#")
+    print(f"# Procesador")
+    print(f"# {plataforma.processor}")
+    print(f"#")
+    print(f"# Maquina")
+    print(f"# {plataforma.machine}")
+    print(Fore.RESET)
+
+def cpu():
+    limpiar()
+    infobasica()
+    cpu = psutil.cpu_freq()
+    print(Fore.LIGHTGREEN_EX)
+    print("<" + "=" * 39 + " CPU Info " + "=" * 39 + ">")
+    print("#")
+    print(f"# Nucleos Fisicos")
+    print(f"# {psutil.cpu_count(logical= False)}")
+    print(f"#")
+    print(f"# Nucleos Totales")
+    print(f"# {psutil.cpu_count(logical=True)}")
+    print(f"#")
+    print("# Maxima Frecuencia del CPU")
+    print(f"# {cpu.max: .2f}Mhz")
+    print("#")
+    print("# Minima Frecuencia del CPU")
+    print(f"# {cpu.min: .2f}Mhz")
+    print("#")
+    print("# Frecuencia Actual")
+    print(f"# {cpu.current: .2f}Mhz")
+    print("#")
+    print(Fore.RESET)
+    
+def memoria():
+    limpiar()
+    memory = psutil.virtual_memory()
+    disponible = memory.total
+    print(f"{Fore.LIGHTGREEN_EX}")
+    print("<" + "=" * 39 + " Memory Info " + "=" * 39 + ">")
+    print("#")
+    print("# Total ")
+    print(f"# {memory.total :,}")
+    print("# Disponible")
+    print(f"# {disponible :,}")
+    print("#")
+    print("# ")
+
+memoria()
